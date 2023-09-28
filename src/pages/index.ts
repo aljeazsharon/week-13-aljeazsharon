2d7c95d76d126caac37ac6cd62b7c7257f7b31f2
aljeazsharon <aljeazsharon@gmail.com>
2023-09-29 01:58:42 +0700

    Update index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,12 +1,3 @@
-// export { default as Home } from './Home';
-// export { default as Product } from './Product';
-// export { default as Profile } from './Profile';
-// export { default as ProductDetail } from './ProductDetail';
-// export { default as ProductNew } from './ProductNew';
-// export { default as ProductEdit } from './ProductEdit';
-// export { default as Login } from './Login'; 
-// export { default as Register } from "./Register";
-
 export { default as CategoryList } from "./CategoryList";
 export { default as CategoryEdit } from "./CategoryEdit";
 export { default as CategoryNew } from "./CategoryNew";
