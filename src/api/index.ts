0cfff0ee9d14c2ec8f5d1cc3d3b241ab750d0c8c
Sharon Aljeazsa L Silalahi <aljeazsharon@gmail.com>
2023-09-29 02:50:59 +0700

    add

---
+++ src/api/index.ts
@@ -0,0 +1,37 @@
+import { GetCategoryResponse, FormCategory, Category } from '../types'
+
+export const getCategory = async (): Promise<GetCategoryResponse>  => {
+    const fetching = await fetch('https://mock-api.arikmpt.com/api/category/');
+
+    return fetching.json()
+}
+
+export const deleteCategory = async (id?: number): Promise<void> => {
+    try {
+        const fetching = await fetch(`https://mock-api.arikmpt.com/api/category/${id}`, {
+            method: 'DELETE'
+        })
+
+        return fetching.json()
+
+    } catch (error) {
+        alert(error)
+    }
+}
+
+export const addCategory = async (body: FormCategory) => {
+    try {
+        const fetching = await fetch('https://mock-api.arikmpt.com/api/category/create', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(body)
+        })
+        const response : Promise<Category> = fetching.json()
+        return response
+
+    } catch (error) {
+        alert(error)
+    }
+}
